f8678c1054afdf8909f377611c91db1149d83789
Silent <zdanio95@gmail.com>
2020-01-15 22:47:34 +0100

    Uncap minigames which did not have to be uncapped

--- source/SilentPatchYK2.cpp
+++ source/SilentPatchYK2.cpp
@@ -7,12 +7,88 @@
 #include <windows.h>
 #include "Utils/MemoryMgr.h"
 #include "Utils/Trampoline.h"
+#include "Utils/Patterns.h"
 
 TrampolineMgr trampolines;
 
 static void InitASI()
 {
-	// Patches go here
+	std::unique_ptr<ScopedUnprotect::Unprotect> Protect = ScopedUnprotect::UnprotectSectionOrFullModule( GetModuleHandle( nullptr ), ".text" );
+
+	using namespace Memory;
+	using namespace hook;
+
+	
+	// Make frame limiter busy loop so it's more accurate
+	{
+		auto oneUsCompare = get_pattern( "48 2B C8 48 81 F9 E8 03 00 00", 10 );
+		Patch<uint8_t>( oneUsCompare, 0xEB ); // jl -> jmp
+	}
+
+
+	// Uncap minigames which did not need to be capped
+	{
+		void* noppedFpsCalls[] = {
+			// Karaoke
+			 get_pattern( "41 8D 4C 24 3C E8 ? ? ? ? B2 01", 5 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 41 B8 ? ? ? ? 48 8D 54 24 ? 48 8B CB E8 ? ? ? ? 90 4C 8B C0", 2 ),
+
+			 // Mahjong
+			 get_pattern( "8D 48 3C E8 ? ? ? ? 33 C9", 3 ),
+			 get_pattern( "E8 ? ? ? ? 90 49 8B CE 4C 8D 5C 24 ? 49 8B 5B 30" ),
+
+			 // Oicho Kabu
+			 get_pattern( "B9 ? ? ? ? E8 ? ? ? ? 33 C0 48 89 83 00 07 00 00", 5 ),
+			 get_pattern( "E8 ? ? ? ? 90 89 2D" ),
+
+			 // Cee-lo/chinchiro
+			 get_pattern( "E8 ? ? ? ? 90 48 8B 8B ? ? ? ? 48 85 C9 74 16" ),
+			 get_pattern( "B9 ? ? ? ? E8 ? ? ? ? 90 48 8D 4C 24 48", 5 ),
+
+			 // Tougijyo (Colliseum) menu
+			 get_pattern( "8D 4B 3C E8 ? ? ? ? 48 8D 05", 3 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 33 D2", 2 ),
+
+			 // Batting center
+			 get_pattern( "8D 4D 3C E8 ? ? ? ? 49 8B 46 28", 3 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 48 8B 87 B0 00 00 00", 2 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 48 8B CB 48 8B 5C 24 78", 2 ),
+
+			 // Blackjack
+			 get_pattern( "8D 4B 3C E8 ? ? ? ? 48 8B CF", 3 ),
+			 // The other one is below
+
+			 // Poker
+			 get_pattern( "8D 4B 3C E8 ? ? ? ? B9 40 01 00 00", 3 ),
+			 // The other one is below
+
+			 // Hanafuda/Koi-Koi
+			 get_pattern( "8D 4E 3C E8 ? ? ? ? 89 B3 80 00 00 00", 3 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 48 8B 5C 24 68", 2 ),
+
+			 // Unknown baseball game
+			 get_pattern( "8D 4B 3C E8 ? ? ? ? 48 8B 46 28", 3 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 48 8B 43 28", 2 ),
+
+			 // Unknown baseball game #2
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 48 8D 93 1C 03 00 00", 2 ),
+			 get_pattern( "48 89 91 50 03 00 00 B9 ? ? ? ? E8 ?", 12 ),
+
+			 // Darts
+			 get_pattern( "B9 ? ? ? ? E8 ? ? ? ? 48 8D 05 ? ? ? ? 48 89 44 24 58", 5 ),
+			 get_pattern( "33 C9 E8 ? ? ? ? 90 48 8D 8E 30 08 00 00", 2 )
+		};
+
+		for ( void* addr : noppedFpsCalls )
+		{
+			Nop( addr, 5 );
+		}
+
+		// Blackjack and poker dtors
+		pattern( "33 C9 E8 ? ? ? ? 90 48 8B CB 48 8B 5C 24 40" ).count(2).for_each_result( [] ( pattern_match match ) {
+			Nop( match.get<void>( 2 ), 5 );
+		} );
+	}
 }
 
 // ========== Hooking boiletplate ==========
