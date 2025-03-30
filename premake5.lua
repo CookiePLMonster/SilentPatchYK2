5854bb94b481970ffd8b91d438e67f2ec9fea9e8
Silent <zdanio95@gmail.com>
2025-03-30 11:22:45 +0200

    Update submodules, premake project, copyright dates, add CI

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,47 @@
+-- Custom API additions START
+require('vstudio')
+
+premake.api.register {
+	name = "vcpkg",
+	scope = "config",
+	kind = "boolean"
+}
+premake.api.register {
+	name = "vcpkgmanifest",
+	scope = "config",
+	kind = "boolean"
+}
+premake.api.register {
+	name = "vcpkgstatic",
+	scope = "config",
+	kind = "boolean"
+}
+premake.api.register {
+	name = "vcpkgconfig",
+	scope = "config",
+	kind = "string"
+}
+
+premake.override(premake.vstudio.vc2010.elements, "configurationProperties", function(base, cfg)
+	local calls = base(cfg)
+	table.insert(calls, function(cfg)
+		if cfg.vcpkg ~= nil then
+			premake.w('<VcpkgEnabled>%s</VcpkgEnabled>', cfg.vcpkg)
+		end
+		if cfg.vcpkgmanifest ~= nil then
+			premake.w('<VcpkgEnableManifest>%s</VcpkgEnableManifest>', cfg.vcpkgmanifest)
+		end
+		if cfg.vcpkgstatic ~= nil then
+			premake.w('<VcpkgUseStatic>%s</VcpkgUseStatic>', cfg.vcpkgstatic)
+		end
+		if cfg.vcpkgconfig ~= nil then
+			premake.w('<VcpkgConfiguration>%s</VcpkgConfiguration>', cfg.vcpkgconfig)
+		end
+	end)
+	return calls
+end)
+-- Custom API additions END
+
 workspace "SilentPatchYK2"
 	platforms { "Win64" }
 
@@ -6,12 +50,12 @@ project "SilentPatchYK2"
 	targetextension ".asi"
 	language "C++"
 
-	include "source/VersionInfo.lua"
-	files { "**/MemoryMgr.h", "**/Trampoline.h", "**/Patterns.*" }
+	dofile "source/VersionInfo.lua"
+	files { "**/MemoryMgr.h", "**/Trampoline.h", "**/Patterns.*", "**/HookInit.hpp" }
 
 
 workspace "*"
-	configurations { "Debug", "Release", "Master" }
+	configurations { "Debug", "Release", "Shipping" }
 	location "build"
 
 	vpaths { ["Headers/*"] = "source/**.h",
@@ -21,9 +65,12 @@ workspace "*"
 
 	files { "source/*.h", "source/*.cpp", "source/resources/*.rc" }
 
+	-- Disable exceptions in WIL
+	defines { "WIL_SUPPRESS_EXCEPTIONS" }
+
 	cppdialect "C++17"
 	staticruntime "on"
-	buildoptions { "/permissive-", "/sdl" }
+	buildoptions { "/sdl" }
 	warnings "Extra"
 
 	-- Automated defines for resources
@@ -34,14 +81,14 @@ filter "configurations:Debug"
 	defines { "DEBUG" }
 	runtime "Debug"
 
- filter "configurations:Master"
-	defines { "NDEBUG" }
-	symbols "Off"
+ filter "configurations:Shipping"
+	defines { "NDEBUG", "RESULT_DIAGNOSTICS_LEVEL=0", "RESULT_INCLUDE_CALLER_RETURNADDRESS=0" }
+	linkoptions { "/pdbaltpath:%_PDB%" }
 
 filter "configurations:not Debug"
 	optimize "Speed"
 	functionlevellinking "on"
-	flags { "LinkTimeOptimization" }
+	linktimeoptimization "on"
 
 filter { "platforms:Win32" }
 	system "Windows"
@@ -50,3 +97,11 @@ filter { "platforms:Win32" }
 filter { "platforms:Win64" }
 	system "Windows"
 	architecture "x86_64"
+
+filter { "toolset:*_xp"}
+	defines { "WINVER=0x0501", "_WIN32_WINNT=0x0501" } -- Target WinXP
+	buildoptions { "/Zc:threadSafeInit-" }
+
+filter { "toolset:not *_xp"}
+	defines { "WINVER=0x0601", "_WIN32_WINNT=0x0601" } -- Target Win7
+	conformancemode "on"
--- source/VersionInfo.lua
+++ source/VersionInfo.lua
@@ -1,8 +1,9 @@
 defines { 
     "rsc_FullName=\"SilentPatch for Yakuza Kiwami 2\"",
     "rsc_MinorVersion=0",
-    "rsc_RevisionID=1",
+    "rsc_RevisionID=2",
     "rsc_BuildID=0",
-    "rsc_Copyright=\"2020\"",
-    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchYK2/releases\""
+    "rsc_Copyright=\"2020-2025\"",
+    "rsc_UpdateURL=\"https://github.com/CookiePLMonster/SilentPatchYK2/releases\"",
+	"rsc_CompanyName=\"Silent\""
 }
